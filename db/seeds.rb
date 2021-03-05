77bdb72f17a2ea218aaac58d578893384b58d43f
Shaqqour <mshaqqourm@gmail.com>
2021-03-04 20:05:02 -0800

    Adds some data to the seed file for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+movies = Movie.create([{ title: 'Star Wars', director: "George Lucas", year: "2008" }, { title: 'Lord of the Rings', director: "Peter Jackson", year: "2001" }, { title: 'E.T.', director: "Steven Spielberg", year: "1982" } ])
